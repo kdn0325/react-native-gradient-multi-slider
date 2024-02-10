ba11f1bc73d97281f7e950909a4eb9b608440640
kdn0325 <dn10003@gmail.com>
2024-02-10 19:21:11 +0900

    feat: AOS/IOS package add

---
+++ index.ios.js
@@ -0,0 +1,67 @@
+import React, { Component, createRef } from 'react';
+import { processColor } from 'react-native';
+
+import NativeLinearGradient from './src';
+
+const convertPoint = (name, point) => {
+  if (Array.isArray(point)) {
+    console.warn(
+      `LinearGradient '${name}' property should be an object with fields 'x' and 'y', ` +
+        'Array type is deprecated.',
+    );
+
+    return {
+      x: point[0],
+      y: point[1],
+    };
+  }
+  return point;
+};
+
+export default class LinearGradient extends Component {
+  constructor(props) {
+    super(props);
+    this.gradientRef = createRef();
+  }
+
+  static defaultProps = {
+    start: { x: 0.5, y: 0.0 },
+    end: { x: 0.5, y: 1.0 },
+  };
+
+  setNativeProps(props) {
+    this.gradientRef.current.setNativeProps(props);
+  }
+
+  render() {
+    const {
+      start,
+      end,
+      colors,
+      locations,
+      useAngle,
+      angleCenter,
+      angle,
+      ...otherProps
+    } = this.props;
+    if (colors && locations && colors.length !== locations.length) {
+      console.warn(
+        'LinearGradient colors and locations props should be arrays of the same length',
+      );
+    }
+
+    return (
+      <NativeLinearGradient
+        ref={this.gradientRef}
+        {...otherProps}
+        startPoint={convertPoint('start', start)}
+        endPoint={convertPoint('end', end)}
+        colors={colors.map(processColor)}
+        locations={locations ? locations.slice(0, colors.length) : null}
+        useAngle={useAngle}
+        angleCenter={convertPoint('angleCenter', angleCenter)}
+        angle={angle}
+      />
+    );
+  }
+}
